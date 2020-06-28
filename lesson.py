0d716d2e6eb22e1057a3878e9444da012f290525
York Liu <york0414@gmail.com>
2020-06-28 20:23:24 +0800

    new stuff

---
+++ lesson.py
@@ -0,0 +1,27 @@
+
+name= input ('請輸入您的名子')
+
+print('嗨！ ', name, '歡迎您的到來')
+
+gen= input ('男生女生')
+print(gen)
+
+age= input ('年紀')
+print(age,'歲')
+
+height= input ('身高')
+print(height,'公分')
+
+weight= input ('體重')
+print(weight,'公京')
+
+print ('謝謝',name,'您的填寫', '以下是您的訊息請確認',
+
+'性別:  ',gen, ',年紀:',age,'歲,','身高:' ,height, '公分,','體重:',weight, 'kg',)
+
+
+
+
+
+
+
